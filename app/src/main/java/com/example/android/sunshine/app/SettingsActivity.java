c352fc26f35a0ad195bb276a36f52f955250a132
OwenLaRosa <owen.larosa@outlook.com>
2016-08-24 14:31:45 -0400

    feat: Add preference for flat icon pack

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -49,6 +49,7 @@ public class SettingsActivity extends PreferenceActivity
         // updated when the preference changes.
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_art_pack_key)));
     }
 
     // Registers a shared preference change listener that gets notified when preferences change
@@ -133,8 +134,10 @@ public class SettingsActivity extends PreferenceActivity
         } else if ( key.equals(getString(R.string.pref_units_key)) ) {
             // units have changed. update lists of weather entries accordingly
             getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
+        } else if (key.equals(R.string.pref_art_pack_key)) {
+            getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
         }
-    }
+     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
